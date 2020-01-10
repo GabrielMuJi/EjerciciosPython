3572e44f37062aedf1a7d73d65fb693236dc282c
GabrielMuJi <krad_leirbag@hotmail.com>
2020-01-10 10:17:01 +0100

    Segundo commit de código

---
+++ 01Ejercicio2.py
@@ -0,0 +1,9 @@
+"""
+Ejercicio 02
+
+Escribir un ciclo definido que salude por pantalla a sus cinco mejores
+amigos/as.
+"""
+
+for amigo in ["Alejandro","Nando","Yisu","Paquito","HontoBonto"]:
+    print("HOLA "+amigo+" K ASE KBSA")
